763165dc1f7278a8bf27dc31fcc4bfcf9ed09643
Franc201 <maxlurie2004@icloud.com>
2025-03-04 18:34:32 +0100

    Lab1(praca w klasie)

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point p1 = new Point();
+        p1.setX(5);
+        p1.setY(6);
+
+        Point p2 = new Point(7,8);
+        System.out.println(p1.toSvg());
+        System.out.println();
+        System.out.println(p2.toSvg());
+
+    }
+}
