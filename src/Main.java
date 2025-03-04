eb501ddcd8b6373aaadf627878b31650b8c27885
Franc201 <maxlurie2004@icloud.com>
2025-03-04 19:45:19 +0100

    Lab2(praca w klasie)

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,17 @@ public class Main {
         p1.setY(6);
 
         Point p2 = new Point(7,8);
-        System.out.println(p1.toSvg());
-        System.out.println();
-        System.out.println(p2.toSvg());
+
+        Point p3 = new Point(p1);
+        Point p4 = new Point(p2);
+
+        Segment s = new Segment(p1,p2);
+        Segment s2 = new Segment(p3,p4);
+        p1.setX(15);
+        p1.setY(12);
+        p2.setX(15);
+        p2.setY(8);
+
 
     }
 }
---
+++ src/Polygon.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Polygon {
+
+    ArrayList<Point> points;
+
+
+}
